3b82371c855b9d6e7044d1cdaad4ff5e79bb22d2
unknown <Administrator@LDL-SANJUKTA.impelsys.com>
2019-07-23 15:36:13 +0530

    using react router dom with the home page animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import HomePage from './HomePage.component';
+import { Route,Switch,Link } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>Hats Page....</h1>
+      <Link to="/">Go back to Home page</Link>
+    </div>
+  )
+  } 
+  
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
